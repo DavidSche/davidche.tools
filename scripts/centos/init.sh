ed8fd9942498f33235910f5bb56414137cdf9902
che_shuai <che_shuai@massclouds.com>
2019-05-29 16:31:34 +0800

    update

--- scripts/centos/init.sh
+++ scripts/centos/init.sh
@@ -130,6 +130,7 @@ chmod +x /usr/local/bin/docker-compose
 #
 #curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 #chmod +x /usr/local/bin/docker-compose
+# scp 192.168.9.20:/usr/local/bin/docker-compose  /usr/local/bin/
 #
 echo "install docker-compose ok !"
 
